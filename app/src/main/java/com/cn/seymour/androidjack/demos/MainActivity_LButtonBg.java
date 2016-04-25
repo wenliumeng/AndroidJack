bc9b7bbe2ee1997796b60f409bf3849e56315a40
seyMour <120465271@qq.com>
2016-04-25 11:53:01 +0800

    LButtonBg activity and layout

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_LButtonBg.java
@@ -0,0 +1,46 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * Lbutton
+ * @author Administrator
+ *
+ */
+public class MainActivity_LButtonBg extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lbuttonbg);
+		LButtonBg bt = (LButtonBg)findViewById(R.id.btn);
+		if (bt != null) {
+			bt.setBackgroundColor(Color.BLUE, Color.CYAN, -10, -10, -10, 4, 2, Color.RED);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}

df206dc0dc905425e4f5a37076846d1beb65993d
seyMour <120465271@qq.com>
2016-05-26 09:16:52 +0800

    scroll view

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_ScrollView.java
@@ -0,0 +1,23 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * ScrollView
+ * @author Administrator
+ *
+ */
+public class MainActivity_ScrollView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_scrollview);
+	}
+
+	
+}

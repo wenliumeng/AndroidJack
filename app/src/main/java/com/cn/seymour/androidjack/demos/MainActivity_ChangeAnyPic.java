d6090752084aa9068cea1dbe046e5be9eb509789
seyMour <120465271@qq.com>
2016-06-13 08:53:46 +0800

    change any picture

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_ChangeAnyPic.java
@@ -0,0 +1,25 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * �任意改变图片
+ * 
+ * MatrixImageView
+ * 
+ * @author Administrator
+ *
+ */
+public class MainActivity_ChangeAnyPic extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_changeanypic);
+	}
+
+}

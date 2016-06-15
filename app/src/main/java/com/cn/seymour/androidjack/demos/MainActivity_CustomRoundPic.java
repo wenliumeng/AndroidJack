151ae30429ace1d3ae87a924830fd436c21528d6
seyMour <120465271@qq.com>
2016-06-15 08:47:37 +0800

    custom round picture

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_CustomRoundPic.java
@@ -0,0 +1,22 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * �自定义控件，圆角图片，CustomImageView
+ * @author Administrator
+ *
+ */
+public class MainActivity_CustomRoundPic extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_customroundpic);
+	}
+
+}

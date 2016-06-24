66271c9a96d859eef27d3e66976b76c3f7134888
seyMour <120465271@qq.com>
2016-06-24 09:39:31 +0800

    custom widget

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_CustomWidgetDrawComplexPic.java
@@ -0,0 +1,18 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+public class MainActivity_CustomWidgetDrawComplexPic extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_customwidgetdrawcomplexpic);
+	}
+
+	
+}

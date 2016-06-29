38ed9e47342fbfba7aa091c74c7d6ac1290967fa
seyMour <120465271@qq.com>
2016-06-29 09:08:06 +0800

    GuaGuaKa activity

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_GuaGuaKa.java
@@ -0,0 +1,22 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * 刮刮卡
+ *
+ * @author Administrator
+ */
+public class MainActivity_GuaGuaKa extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_guaguaka);
+    }
+
+}

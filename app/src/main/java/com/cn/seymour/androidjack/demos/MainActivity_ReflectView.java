1166dfd8da0bc11a9815bc26dcad07540b9decfb
seyMour <120465271@qq.com>
2016-07-18 08:52:26 +0800

    ReflectView

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_ReflectView.java
@@ -0,0 +1,25 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * 滤镜 ReflectView,暗淡效果
+ * @author Administrator
+ *
+ */
+public class MainActivity_ReflectView extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_reflectview);
+	}
+
+	
+	
+}

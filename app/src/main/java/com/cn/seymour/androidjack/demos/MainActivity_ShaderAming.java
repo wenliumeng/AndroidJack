f34c912b4d48d9d96f85f4287e8e16e152aa92ec
seyMour <120465271@qq.com>
2016-08-10 09:16:44 +0800

    ShaderAming activity

---
+++ app/src/main/java/com/cn/seymour/androidjack/demos/MainActivity_ShaderAming.java
@@ -0,0 +1,23 @@
+package com.cn.seymour.androidjack.demos;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cn.seymour.androidjack.R;
+
+/**
+ * shader做出移动瞄准镜的效果
+ * @author Administrator
+ *
+ */
+public class MainActivity_ShaderAming extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_shaderaming);
+	}
+
+	
+}
